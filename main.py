4584c5dba47dd9f00da1d3b69530179036677374
tjthejuggler <Github183630>
2022-02-17 21:04:11 -0600

    todo

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-#detect first X keypress positions
-#pick random amount of minutes around 120
-#	use this number to set the amount of time for the first click
-#	give all other times a random small amount after that and then for each of the rest
-#do the mouse clicks at their times
-#then go back to 2
-
-
-#between wakeups, do in/out of game clicks to prevent timeout
+#when i go to wake up, i could also put a few to sleep so that i am more likely to always have some awake
+#slightly randomize positions every click
 
 import keyboard
-
 import pyautogui
 import time
 import math
 import random
 
+base_session_minutes = 7228
 
 def getWeightedRandom(min, max): #(less likely to be near min i think)
 	ourRand = random.random()
@@ -24,27 +17,17 @@ def getWeightedRandom(min, max): #(less likely to be near min i think)
 	print("getWeightedRandom", number)
 	return number
 
-base_session_minutes = 7228
-
-getWeightedRandom(0, 3000)
-
-
 click_pos = []
 keyboard.wait('p')
 click_pos.append(pyautogui.position())
-print(pyautogui.position())
-keyboard.wait('p')
-click_pos.append(pyautogui.position())
-print(pyautogui.position())
 keyboard.wait('p')
 click_pos.append(pyautogui.position())
 keyboard.wait('p')
 click_pos.append(pyautogui.position())
 keyboard.wait('p')
 click_pos.append(pyautogui.position())
-pyautogui.click(20, 100)
-
-# pyautogui.moveRel(0, 10) 
+keyboard.wait('p')
+click_pos.append(pyautogui.position())
 
 while True:
 	this_session_minutes = base_session_minutes + getWeightedRandom(0,978) + random.random()
@@ -55,7 +38,3 @@ while True:
 		time.sleep(abs(between_click_time))
 		print(i, between_click_time)
 		pyautogui.click(i)
-		
-
-#todo
-#take screenshot(or use one took) and make image.png with small item
