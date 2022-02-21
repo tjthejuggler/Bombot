94733f708f6001c8e7368a17cbd42276409159c3
tjthejuggler <Github183630>
2022-02-21 16:31:25 -0600

    saving capability added

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ import time
 import math
 import random
 import sys
+import re
+from shapely.geometry import Point
+
+from pynput.keyboard import Key, Listener
+  
+
+  
+
 
 def getWeightedRandom(min, max): #(less likely to be near min i think)
 	ourRand = random.random()
@@ -19,41 +27,52 @@ def getWeightedRandom(min, max): #(less likely to be near min i think)
 	print("getWeightedRandom", number)
 	return number + random.random()
 
-base_session_minutes = 828
+def write_to_file(click_pos_before):
+	print("write to file", click_pos_before)
+	with open('click_pos_before.txt', 'w') as filehandle:
+		for listitem in click_pos_before:
+			filehandle.write('%s\n' % str(listitem))
+	print("write to file", click_pos_before)
+
+def read_from_file():
+	print("read to file1",click_pos_before)
+	click_pos_before_loaded = []
+	with open('click_pos_before.txt', 'r') as filehandle:
+		for line in filehandle:
+			# remove linebreak which is the last character of the string
+			currentPlace = line[:-1]
+			print('currentPlace', currentPlace)
+			#coords = [int(s) for s in currentPlace.split() if s.isdigit()]
+			coords = re.findall(r'\d+', currentPlace)
+			print('coords', coords)
+			# add item to the list
+			if len(coords) > 1:					
+				class myPoint(object):
+				    pass
+				mp = myPoint()
+				mp.x = int(coords[0])
+				mp.y = int(coords[1])
+				click_pos_before_loaded.append(mp)
+			print(click_pos_before_loaded)
+	print("read to file2",click_pos_before)
+	return click_pos_before_loaded
+
+base_session_minutes = 10
 inProgrammingMode = True
 current_sequence_number = 0
 
 click_pos_before = []
-click_pos_1 = []
-click_pos_2 = []
-click_pos_3 = []
+click_pos_mid = [[]]*3
 click_pos_after = []
-
-while inProgrammingMode:
-	print('inProg')
-	if keyboard.read_key() == "a":
-		click_pos_before.append(pyautogui.position())
-	# if keyboard.read_key() == "1":
-	# 	click_pos_.mid[0].append(pyautogui.position())		
-	# if keyboard.read_key() == "2":
-	# 	click_pos_.mid[1].append(pyautogui.position())
-	# if keyboard.read_key() == "3":
-	# 	click_pos_.mid[2].append(pyautogui.position())
-	# if keyboard.read_key() == "s":
-	# 	click_pos_after.append(pyautogui.position())
-	if keyboard.read_key() == "d":
-		print('d pressed')
-		inProgrammingMode = False
-
-		
-while not inProgrammingMode:
-	this_session_minutes = base_session_minutes + getWeightedRandom(0,321)
+def sequence_loop():
+#while not inProgrammingMode:
+	this_session_minutes = base_session_minutes + getWeightedRandom(0,1)
 	print('this_session_minutes', this_session_minutes, click_pos_before)
 
 	for i in range(int(this_session_minutes),0,-1):
-	    sys.stdout.write(str(i)+' ')
-	    sys.stdout.flush()
-	    time.sleep(1)
+		sys.stdout.write(str(i)+' ')
+		sys.stdout.flush()
+		time.sleep(1)
 	#time.sleep(this_session_minutes)
 	for pos in click_pos_before:
 		print(pos)
@@ -64,6 +83,7 @@ while not inProgrammingMode:
 	for idx, pos in enumerate(click_pos_mid[current_sequence_number]): #this should be able to iterate through list items and have their index
 		between_click_time = getWeightedRandom(0,4)
 		time.sleep(abs(between_click_time))
+		#ONLY DO MOVE AND DRAG IF WE ARE ON 1 OR 2 AND ON 2 DO IT TWICE
 		if idx == 0:
 			pyautogui.moveTo(pos.x+getWeightedRandom(0,4), pos.y+getWeightedRandom(0,4))
 		elif idx == 1:
@@ -82,6 +102,65 @@ while not inProgrammingMode:
 	current_sequence_number =+ 1
 	if current_sequence_number == 3:
 		current_sequence_number = 0
+
+def show(key):
+	global click_pos_before
+	global click_pos_mid
+	global click_pos_after
+	# by pressing 'delete' button 
+	# you can terminate the loop 
+	if key == Key.delete: 
+		return False	
+	if hasattr(key,'char'):
+		if key.char == ('a'):
+			print('new a')
+			click_pos_before.append(pyautogui.position())
+			print(click_pos_before) 	  
+		if key.char == ('1'):
+			click_pos_mid[0].append(pyautogui.position())
+		if key.char == ('2'):
+			click_pos_mid[1].append(pyautogui.position())
+		if key.char == ('3'):
+			click_pos_mid[2].append(pyautogui.position())
+		if key.char == ('s'):
+			click_pos_after.append(pyautogui.position())
+		if key.char == ('d'):
+			# inProgrammingMode = False
+			sequence_loop()
+		if key.char == ('w'):
+			print('w pressed')
+			write_to_file(click_pos_before)
+		if key.char == ('r'):
+			print('r pressed')
+			click_pos_before = read_from_file()
+			print("read to file3",click_pos_before)
+# Collect all event until released
+with Listener(on_press = show) as listener:
+	listener.join()
+
+# while inProgrammingMode:
+# 	print('inProg')
+# 	if keyboard.read_key() == "a":
+# 		click_pos_before.append(pyautogui.position())
+# 		print(click_pos_before)
+# 	if keyboard.read_key() == "1":
+# 		click_pos_mid[0].append(pyautogui.position())		
+# 	if keyboard.read_key() == "2":
+# 		click_pos_mid[1].append(pyautogui.position())
+# 	if keyboard.read_key() == "3":
+# 		click_pos_mid[2].append(pyautogui.position())
+# 	if keyboard.read_key() == "s":
+# 		click_pos_after.append(pyautogui.position())
+# 	if keyboard.read_key() == "d":
+# 		print('d pressed')
+# 		inProgrammingMode = False
+# 	if keyboard.read_key() == "w":
+# 		print('w pressed')
+# 		write_to_file(click_pos_before)
+# 	if keyboard.read_key() == "r":
+# 		print('r pressed')
+# 		click_pos_before = read_from_file()
+
 
 		
 
